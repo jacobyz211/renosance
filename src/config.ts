ad6e0e33455fb8ea1f1c630157e1d3da84131a0e
jacobyz211 <cyrusna29@icloud.com>
2026-06-01 19:07:53 -0400

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,35 @@
+export const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/124.0.0.0 Safari/537.36';
+export const TIMEOUT_MS = 12000;
+export const QOBUZ_BASE = 'https://www.qobuz.com/api.json/0.2';
+export const DEEZER_API = 'https://api.deezer.com';
+
+export const QOBUZ_INSTANCES = [
+  'https://qobuz-api1.onrender.com',
+  'https://trypt-hifi-dl-456461932686.us-west1.run.app',
+  'https://qobuz-api.stremio123.duckdns.org',
+  'https://qobuz.kennyy.com.br/api',
+];
+
+export const DEFAULT_HIFI_INSTANCES = [
+  'https://hifi-api-bffw.onrender.com',
+  'https://hifi-api-workers.anothermoumen4.workers.dev',
+  'https://hifi-api-pj08.onrender.com',
+  'https://mono.kennyy.com.br/hifi-api',
+  'https://tidal-api.binimum.org',
+  'https://triton.squid.wtf',
+  'https://ohio-1.monochrome.tf',
+  'https://frankfurt-1.monochrome.tf',
+  'https://vogel.qqdl.site',
+  'https://eu-central.monochrome.tf',
+  'https://us-west.monochrome.tf',
+  'https://monochrome-api.samidy.com',
+  'https://hifi-two.spotisaver.net',
+  'https://wolf.qqdl.site',
+  'https://katze.qqdl.site',
+  'https://hund.qqdl.site',
+  'https://api.monochrome.tf',
+];
+
+export const QUALITY_FORMAT_MAP: Record<string, number> = {
+  MP3: 5, CD: 6, HIRES_96: 7, HIRES: 27,
+};
